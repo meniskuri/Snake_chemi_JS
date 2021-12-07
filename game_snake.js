a2df2001e9ee9b666b86d164a8200a3c7a271f1d
Meniskuri <giorgikapanadze9169@gmail.com>
2021-12-07 18:49:18 +0400

    mariamis kodis gawmenda gadaxedva garcheva - naxevari ver gavige

--- game_snake.js
+++ game_snake.js
@@ -1,43 +1,50 @@
-const canvas = document.querySelector('#canvas');
-const context = canvas.getContext('2d');
+const canvas     = document.querySelector('#canvas');
+const context    = canvas.getContext('2d');
 const squareSize = 50;
 const targetSize = 20;
-const moveSpeed = 1;
-const play = document.querySelector('#play');
+const moveSpeed  = 1;
+const play       = document.querySelector('#play');
 // let gameTime = 30;
 
-let squareX = 50;
-let squareY = 100;
+let squareX = 0; // თავის საწყისი პოზიცია იქსი
+let squareY = 0; // იგრეკი
 
-let targetX = 0;
+let targetX = 0; // ვაშლი
 let targetY = 0;
 
-let up = false;
-let down = false;
-let right = false;
-let left = false;
+let up      = false;
+let down    = false;
+let right   = false;
+let left    = false;
 
-var point = 0;
+var point   = 0;
 var pointIs = document.getElementById('point');
 
 
-play.addEventListener('click', e => {
+play.addEventListener('click', e =>
+{
   canvas.style.display = 'block';
   // timer = setInterval(updateTimer, 1000);
   // updateTimer();
   // $('#playAgainButton').hide();
   var count = 30;
 
-  function startGame() {
+  function startGame()
+  { // startGame დასაწყისი
+
     moveTarget();
     draw();
     moveSquare();
+
     point = 0;
     timer = 30;
-    var interval = setInterval(function() {
+
+    var interval = setInterval(function()
+    {
       document.getElementById('countdown').innerHTML = count;
       count--;
-      if (count === 0){
+      if (count === 0)
+      {
         clearInterval(interval);
         document.getElementById('countdown').innerHTML='კიდევ სცადე ჩემო კარგო';
         canvas.style.display = 'none';
@@ -45,94 +52,130 @@ play.addEventListener('click', e => {
       clearInterval ();
     }, 1000);
 
-    document.addEventListener('keydown', e => {
-      if (e.key === 'ArrowUp') {
+
+    document.addEventListener('keydown', e =>
+    {
+      if (e.key === 'ArrowUp')
+      {
         up = true;
       }
-      if (e.key === 'ArrowDown') {
+      if (e.key === 'ArrowDown')
+      {
         down = true;
       }
-      if (e.key === 'ArrowRight') {
+      if (e.key === 'ArrowRight')
+      {
         right = true;
       }
-      if (e.key === 'ArrowLeft') {
+      if (e.key === 'ArrowLeft')
+      {
         left = true;
       }
       console.log('keydown', e.key);
       // moveSquare();
     });
-    document.addEventListener('keyup', e => {
-      if (e.key === 'ArrowUp') {
+
+    // ეს რას აკეთებს? :))
+    document.addEventListener('keyup', e =>
+    {
+      if (e.key === 'ArrowUp')
+      {
         up = false;
       }
-      if (e.key === 'ArrowDown') {
+      if (e.key === 'ArrowDown')
+      {
         down = false;
       }
-      if (e.key === 'ArrowRight') {
+      if (e.key === 'ArrowRight')
+      {
         right = false;
       }
-      if (e.key === 'ArrowLeft') {
+      if (e.key === 'ArrowLeft')
+      {
         left = false;
       }
       console.log('keyup', e.key);
     });
-  }
-startGame();
+
+  } // startGame დასასრული?
+
+  startGame();
+
 });
 
 
 
-function draw() {
+function draw()
+{
   resetCanvas();
   // Big square
   context.fillStyle = '#f0ff1d','border';
   context.fillRect(squareX, squareY, squareSize, squareSize);
+
   // Small square
   context.fillStyle = 'hsl(283, 93%, 53%)';
   context.fillRect(targetX, targetY, targetSize, targetSize);
+
   // pointIs.innerHTML = 'შენი ქულა';
   pointIs.innerHTML = point;
   // document.getElementById('point').innerHTML='შენი ქულა იხილე თამაშის ბოლოს';;
-  
 }
 
 
-function moveSquare() {
-  if (up) {
+function moveSquare()
+{
+  if (up)
+  {
     squareY -= moveSpeed;
   }
-  if (down) {
+  if (down)
+  {
     squareY += moveSpeed;
   }
-  if (left) {
+  if (left)
+  {
     squareX -= moveSpeed;
   }
-  if (right) {
+  if (right)
+  {
     squareX += moveSpeed;
   }
-  if (squareX + squareSize > canvas.width) {
+
+  if (squareX + squareSize > canvas.width)
+  {
     squareX = canvas.width - squareSize;
   }
-  if (squareY + squareSize > canvas.height) {
+  if (squareY + squareSize > canvas.height)
+  {
     squareY = canvas.height - squareSize;
   }
+
   squareX = Math.max(0, squareX);
   squareY = Math.max(0, squareY);
-  if(isEaten()){
+
+  if(isEaten())
+  {
     moveTarget();
     point += 1;
   }
+
   draw();
+
   setTimeout(() => moveSquare(), 1 );
 }
 
-function moveTarget() {
+
+function moveTarget()
+{
   const {x, y} = getTargetRandomXY();
+
   targetX = x;
   targetY = y;
 }
 
-function getTargetRandomXY() {
+
+function getTargetRandomXY()
+{
   const x = Math.floor(Math.random() * (canvas.width - targetSize));
   const y = Math.floor(Math.random() * (canvas.height - targetSize));
 
@@ -142,16 +185,22 @@ function getTargetRandomXY() {
   }
 }
 
-function resetCanvas() {
+
+function resetCanvas()
+{
   context.fillStyle = 'white';
   context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-function isEaten(){
+
+function isEaten()
+{
   const squareBottom = squareY + squareSize;
-  const squareRight = squareX + squareSize;
+  const squareRight  = squareX + squareSize;
   const targetBottom = targetY + targetSize;
-  const targetRight = targetX + targetSize;
+  const targetRight  = targetX + targetSize;
+
   return squareBottom > targetBottom && squareY < targetY && squareRight > targetRight && squareX < targetX;
+
   point += 1;
 }
