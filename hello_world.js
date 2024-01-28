d51729918d39f1257ee6a0e0c2568760d99ca014
Meniskuri <giorgikapanadze9169@gmail.com>
2024-01-28 22:01:06 +0400

    vagrdzeleb shesavals js s shi

--- hello_world.js
+++ hello_world.js
@@ -1,1 +1,25 @@
 console.log("gamarjoba dante");
+let a = 2
+let b = 1
+var gio = "g\io"
+
+console.log(gio)
+console.log("/////////////////")
+console.log(a+b);
+
+let c = "gio chkviania\n";
+let d = `gios
+dzmakacia
+dante
+aligieri`
+
+console.log(d)
+console.log(c + a + "\n" + b)
+console.log(c.length)
+
+
+for(let a = 0; a <= c.length; a++) {
+  console.log("gios ragac gamosdis \n" + a)
+}
+
+let person = prompt("what is your name? ")
