67012d6d07e31752946a478752f6a737a9209528
Meniskuri <giorgikapanadze9169@gmail.com>
2024-01-30 20:18:36 +0400

    daviwye davalebis dawera davaleba 1

--- BitCamp_JS_free/tavi_1/tavi1_davaleba.js
+++ BitCamp_JS_free/tavi_1/tavi1_davaleba.js
@@ -1,21 +1,14 @@
 function calculate() {
     // Get the values from the input boxes
-    var num1 = document.getElementById('num1').value;
-    var num2 = document.getElementById('num2').value;
-    var text1 = document.getElementById("text1").value;
-    
+    var text1 = document.getElementById('text1').value;
+    var text2 = document.getElementById('text2').value;
 
     // Perform the calculation (here, addition is used as an example)
-    var result = parseFloat(num1) + parseFloat(num2);
-    var result2 = parseFloat(num1) - parseFloat(num2);
-    var result3 = text1 + result + result2;
-    var result4 = text1
+    var result1 = text1 + text2;
+
 
     // Display the result in the output text box
-    document.getElementById('result').value = result;
-    document.getElementById('result2').value = result2;
-    document.getElementById('result3').value = result3;
-    document.getElementById('result4').value = typeof result2;
+    document.getElementById('result1').value = result1;
+    document.getElementById('result2').value = typeof result1;
 }
-
 
---
+++ BitCamp_JS_free/tavi_1/tests1.js
@@ -0,0 +1,6 @@
+var gio = "13";
+console.log(gio)
+console.log(`var gio is ${typeof gio}`);
+
+var dante = Number(gio)
+console.log(dante)
